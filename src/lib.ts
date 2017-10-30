96962616ffe364c30e7037583bd3cd6f04e78935
Paul Betts <paul@paulbetts.org>
2017-10-29 19:00:12 -0700

    Hey it mostly works

---
+++ src/lib.ts
@@ -0,0 +1,73 @@
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+
+import { asyncMap } from '../src/promise-array';
+
+// tslint:disable-next-line:no-var-requires
+const d = require('debug')('cl-apartment-finder');
+
+export function getLinksFromXML(xmlText: string): string[] {
+  const $ = cheerio.load(xmlText, { xmlMode: true, decodeEntities: true });
+
+  let items = Array.from($('link'));
+  return items
+    .map(x => $(x).text().trim())
+    .filter(x => x.length > 1 && !x.match(/\/search\//i));
+}
+
+export function screenScrapeCoordinatesFromCraigslistHtml(contents: string): number[] {
+  const $ = cheerio.load(contents);
+
+  let lat = parseFloat($('#map').attr('data-latitude'));
+  let lng = parseFloat($('#map').attr('data-longitude'));
+  return [lat, lng];
+}
+
+export async function screenScrapeMapFromCoords(latlng: number[]): Promise<{link: string, img: string}> {
+  const url = `https://google.com/search?q=${latlng[0]}%2C${latlng[1]}`;
+  const result = (await axios.get(url)).data;
+
+  const $ = cheerio.load(result);
+  let img = $('a > img')[0];
+
+  d(img);
+
+  return {
+    img: 'https://google.com/' + $(img).attr('src'),
+    link: $(img).parent().attr('href')
+  };
+}
+
+export interface Listing {
+  url: string;
+  img: string | null;
+  coords: number[];
+  mapLink: string | null;
+}
+
+export async function processLinks(rssFeed: string, alreadySeen: Set<string>): Promise<Listing[]> {
+  let rss = (await axios.get(rssFeed)).data;
+  let links = getLinksFromXML(rss);
+
+  links = links.filter(x => {
+    if (alreadySeen.has(x)) { return false; }
+
+    alreadySeen.add(x);
+    return true;
+  });
+
+  let results = await asyncMap(links, async l => {
+    let latlng = screenScrapeCoordinatesFromCraigslistHtml((await axios.get(l)).data);
+    if (isNaN(latlng[0])) {
+      d(`No map coordinates for ${l}`);
+      return { coords: latlng, link: null, img: null };
+    }
+
+    let ret = await screenScrapeMapFromCoords(latlng);
+    return { coords: latlng, img: ret.img, link: ret.link };
+  }, 4);
+
+  let ret: Listing[] = [];
+  results.forEach((v, k) => ret.push({ url: k, img: v.img, mapLink: v.link, coords: v.coords }));
+  return ret;
+}
--- test/lib.ts
+++ test/lib.ts
@@ -1,8 +1,10 @@
 import * as fs from 'fs';
 import { expect } from 'chai';
+
 import './support';
 
-import { getLinksFromXML, screenScrapeCoordinatesFromCraigslistHtml } from '../src/index';
+import { getLinksFromXML, screenScrapeCoordinatesFromCraigslistHtml,
+  processLinks } from '../src/lib';
 
 // tslint:disable-next-line:no-var-requires
 const d = require('debug')('cl-apartment-finder');
@@ -22,5 +24,16 @@ describe('the parser', () => {
     d(result);
     expect(Math.abs(result[0] - 37.771650)).to.be.below(0.001);
     expect(Math.abs(result[1] - (-122.410174))).to.be.below(0.001);
+  });
+});
+
+const rssFeed = 'https://sfbay.craigslist.org/search/apa?availabilityMode=0&bundleDuplicates=1&format=rss&max_price=6000&min_bedrooms=3&postal=94107&search_distance=2';
+
+describe('integration test', function() {
+  this.timeout(30 * 1000);
+
+  it('doesnt explode', async () => {
+    let results = await processLinks(rssFeed, new Set());
+    results.forEach(x => console.log(`${x.url} - ${x.mapLink}(${x.coords})`));
   });
 });
